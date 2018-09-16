dd5442d53713c2726d7d2a5d84cfda74691fa971
msenesm <m.j.w.vanhengstum@hotmail.com>
2018-09-16 18:46:25 +0200

    function

--- TestfilePycharm.py
+++ TestfilePycharm.py
@@ -3,4 +3,8 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
+print('gemaakt vanuit Pycharm')   # Stijn heeft deze line aangepast
+print('stijn heeft deze line toegevoegd')
+print('gemaakt vanuit VS code')
+
 
