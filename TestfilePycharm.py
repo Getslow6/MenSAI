4d322fb9701fdac02b8b1f7c0c2f286cca898560
Getslow6 <43093176+getslow6@users.noreply.github.com>
2018-09-16 18:27:24 +0200

    Test commit

--- TestfilePycharm.py
+++ TestfilePycharm.py
@@ -1,5 +1,5 @@
 # Deze file is vanuit Pycharm aangemaakt.
 
-print('gemaakt vanuit Pycharm')
-
+print('gemaakt vanuit Pycharm')   # Stijn heeft deze line aangepast
+print('stijn heeft deze line toegevoegd')
 print('gemaakt vanuit VS code')
