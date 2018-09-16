b0799000a0973ac78053c0baa375055af7cb8e3e
Stijn <seuren.stijn@gmail.com>
2018-09-16 18:53:34 +0200

    Test commit

--- Main_file.py
+++ Main_file.py
@@ -2,3 +2,13 @@
 #  - de Main file moet wat berekeningen uitvoeren aan de hand van enkele functies die wij los van elkaar gaan maken.
 #  - Mathijs maakt een functie aan die het volgende doet: output = input^2+10
 #  - Stijn maakt een functie aan die het volgende doet: output = sqrt(input-10)
+
+
+
+Input = 1234
+
+
+
+
+
+
