886586a37d1803d821a939ea9e7a9cb50a69f4d4
Stijn <seuren.stijn@gmail.com>
2018-09-16 18:55:47 +0200

    Test commit

--- Main_file.py
+++ Main_file.py
@@ -3,10 +3,12 @@
 #  - Mathijs maakt een functie aan die het volgende doet: output = input^2+10
 #  - Stijn maakt een functie aan die het volgende doet: output = sqrt(input-10)
 
-
+import TestfilePycharm
+import Stijn_lib
 
 Input = 1234
 
+print(stijn_func(add_10(Input)))
 
 
 
