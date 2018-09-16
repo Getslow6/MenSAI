6644006786bbc6a322db50c3ce4556438a83f122
msenesm <m.j.w.vanhengstum@hotmail.com>
2018-09-16 18:39:16 +0200

    stijn

--- TestfilePycharm.py
+++ TestfilePycharm.py
@@ -1,5 +1,6 @@
 # Deze file is vanuit Pycharm aangemaakt.
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
 
-print('gemaakt vanuit Pycharm')   # Stijn heeft deze line aangepast
-print('stijn heeft deze line toegevoegd')
-print('gemaakt vanuit VS code')
+
