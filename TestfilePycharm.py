70fbb146a4ef999c6177928f75765edc9fcb68e4
msenesm <m.j.w.vanhengstum@hotmail.com>
2018-09-16 18:48:51 +0200

    function

--- TestfilePycharm.py
+++ TestfilePycharm.py
@@ -1,10 +1,16 @@
 # Deze file is vanuit Pycharm aangemaakt.
-import numpy as np
-import math as m
-import matplotlib.pyplot as plt
+# import numpy as np
+# import math as m
+# import matplotlib.pyplot as plt
 
 print('gemaakt vanuit Pycharm')   # Stijn heeft deze line aangepast
 print('stijn heeft deze line toegevoegd')
 print('gemaakt vanuit VS code')
 
 
+# Function to add 10
+def add_10(x):
+    x += 10 
+    return x
+
+print(add_10(10))
